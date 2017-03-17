91a337ac4485114c7bed207a128d4d7d840f260c
Michael Maier <maiermic@cip.ifi.lmu.de>
2017-03-17 08:43:21 +0100

    generate two trajectories

--- src/generate_svg.cpp
+++ src/generate_svg.cpp
@@ -9,40 +9,41 @@
 
 #include <boost/geometry/io/svg/svg_mapper.hpp>
 
+namespace bg = boost::geometry;
+
+using point = bg::model::point<double, 2, bg::cs::cartesian>;
+using linestring = bg::model::linestring<point>;
+
 template <typename Geometry1, typename Geometry2>
-void create_svg(std::string const& filename, Geometry1 const& a, Geometry2 const& b)
+void create_svg(std::string const& filename, Geometry1 const& first, Geometry2 const& second)
 {
     typedef typename boost::geometry::point_type<Geometry1>::type point_type;
     std::ofstream svg(filename.c_str());
 
     boost::geometry::svg_mapper<point_type> mapper(svg, 400, 400);
-    mapper.add(a);
-    mapper.add(b);
+    mapper.add(first);
+    mapper.add(second);
 
-    mapper.map(a, "fill-opacity:0.5;fill:rgb(153,204,0);stroke:rgb(153,204,0);stroke-width:2");
-    mapper.map(b, "opacity:0.8;fill:none;stroke:rgb(255,128,0);stroke-width:4;stroke-dasharray:1,7;stroke-linecap:round");
+    const auto first_style = "fill-opacity:0.5;fill:rgb(153,204,0);stroke:rgb(153,204,0);stroke-width:2";
+    const auto second_style = "opacity:0.8;fill:none;stroke:rgb(255,128,0);stroke-width:4;stroke-dasharray:1,7;stroke-linecap:round";
+
+    mapper.map(first, first_style);
+    mapper.map(second, second_style);
+
+    const linestring first_symbol_map_line { {0.0, 1.0}, {0.3, 1.0} };
+    mapper.add(first_symbol_map_line);
+    mapper.map(first_symbol_map_line, first_style);
+    mapper.text(point(0.4, 1.0), "first", "font-size:18");
+
+    const linestring second_symbol_map_line { {0.0, 0.8}, {0.3, 0.8} };
+    mapper.add(second_symbol_map_line);
+    mapper.map(second_symbol_map_line, second_style);
+    mapper.text(point(0.4, 0.8), "second", "font-size:18");
 }
 
 int main()
 {
-    using namespace boost::assign;
-
-
-    boost::geometry::model::ring<boost::geometry::model::d2::point_xy<double> > ring;
-    ring +=
-        boost::geometry::model::d2::point_xy<double>(4.0, -0.5), boost::geometry::model::d2::point_xy<double>(3.5, 1.0),
-        boost::geometry::model::d2::point_xy<double>(2.0, 1.5), boost::geometry::model::d2::point_xy<double>(3.5, 2.0),
-        boost::geometry::model::d2::point_xy<double>(4.0, 3.5), boost::geometry::model::d2::point_xy<double>(4.5, 2.0),
-        boost::geometry::model::d2::point_xy<double>(6.0, 1.5), boost::geometry::model::d2::point_xy<double>(4.5, 1.0),
-        boost::geometry::model::d2::point_xy<double>(4.0, -0.5);
-
-
-     boost::geometry::model::box<boost::geometry::model::d2::point_xy<double> > box;
-     boost::geometry::envelope(ring, box);
-    std::cout
-        << "make_envelope:"
-        << boost::geometry::dsv(box)
-        << std::endl;
-
-    create_svg("make_envelope.svg", ring, box);
+    linestring trajectory_1 {{0.0, 0.0}, {1.0, 0.0}, {1.0, 2.0}};
+    linestring trajectory_2 {{0.0, 0.0}, {0.8, 0.1}, {0.9, 1.9}};
+    create_svg("make_envelope.svg", trajectory_1, trajectory_2);
 }
