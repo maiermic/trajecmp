aff115ff86ff4aebf97bc7eeeb0e95100f68395c
Michael Maier <maiermic@cip.ifi.lmu.de>
2017-04-23 16:26:23 +0200

    compare trajectories using reactive streams

--- example/rx-example.cpp
+++ example/rx-example.cpp
@@ -1,95 +1,101 @@
+#include <vector>
+#include <iostream>
+#include <functional>
+
+#include <boost/geometry.hpp>
+
+#include "logging.hpp"
+#include "../src/trajecmp/functional/functional.hpp"
+#include "../src/trajecmp/distance/modified_hausdorff.hpp"
+#include "../src/trajecmp/geometry/min_bounding_sphere.hpp"
+#include "../src/trajecmp/geometry/vector.hpp"
+#include "../src/trajecmp/transform/translate.hpp"
+#include "../src/trajecmp/transform/scale.hpp"
+
+#include "../src/trajecmp/util/boost_geometry_to_string.hpp"
+#include "TrajectorySvg.hpp"
+
 #include "rxcpp/rx.hpp"
+
 namespace Rx {
     using namespace rxcpp;
     using namespace rxcpp::sources;
     using namespace rxcpp::operators;
     using namespace rxcpp::util;
 }
-using namespace Rx;
 
-#include <regex>
-#include <random>
-using namespace std;
-using namespace std::chrono;
+using point = boost::geometry::model::point<double, 2, boost::geometry::cs::cartesian>;
+using linestring = boost::geometry::model::linestring<point>;
+using Trajectory = linestring;
+using vector = point;
 
-int main()
-{
-    random_device rd;   // non-deterministic generator
-    mt19937 gen(rd());
-    uniform_int_distribution<> dist(4, 18);
+auto has_min_num_points(int min) {
+    return [=](const auto &trajectory) {
+        return trajectory.size() >= min;
+    };
+}
 
-    // for testing purposes, produce byte stream that from lines of text
-    auto bytes = range(0, 10) |
-                 flat_map([&](int i){
-                     auto body = from((uint8_t)('A' + i)) |
-                                 repeat(dist(gen)) |
-                                 as_dynamic();
-                     auto delim = from((uint8_t)'\r');
-                     return from(body, delim) | concat();
-                 }) |
-                 window(17) |
-                 flat_map([](observable<uint8_t> w){
-                     return w |
-                            reduce(
-                                    vector<uint8_t>(),
-                                    [](vector<uint8_t> v, uint8_t b){
-                                        v.push_back(b);
-                                        return v;
-                                    }) |
-                            as_dynamic();
-                 }) |
-                 tap([](vector<uint8_t>& v){
-                     // print input packet of bytes
-                     copy(v.begin(), v.end(), ostream_iterator<long>(cout, " "));
-                     cout << endl;
-                 });
+auto less_than(int upper) {
+    return [=](int value) {
+        return value <= upper;
+    };
+}
 
-    //
-    // recover lines of text from byte stream
-    //
+Rx::rxsub::subject<Trajectory> input_trajectory_subject;
 
-    auto removespaces = [](string s){
-        s.erase(remove_if(s.begin(), s.end(), ::isspace), s.end());
-        return s;
+int main() {
+    logging::is_logging = true;
+
+    auto input_trajectory_stream = input_trajectory_subject.get_observable();
+    auto pattern_trajectory_stream =
+            Rx::observable<>::just(
+                    Trajectory{
+                            {0, 2},
+                            {0, 0},
+                            {1, 0}
+                    }
+            );
+    const auto transform = [](Trajectory &trajectory) {
+        const auto mbs = trajecmp::geometry::min_bounding_sphere(trajectory);
+        return trajecmp::transform::scale_to_const<100>(mbs.radius)(
+                trajecmp::transform::translate_by(trajecmp::geometry::negative_vector_of(mbs.center))(trajectory)
+        );
     };
 
-    // create strings split on \r
-    auto strings = bytes |
-                   concat_map([](vector<uint8_t> v){
-                       string s(v.begin(), v.end());
-                       regex delim(R"/(\r)/");
-                       cregex_token_iterator cursor(&s[0], &s[0] + s.size(), delim, {-1, 0});
-                       cregex_token_iterator end;
-                       vector<string> splits(cursor, end);
-                       return iterate(move(splits));
-                   }) |
-                   filter([](const string& s){
-                       return !s.empty();
-                   }) |
-                   publish() |
-                   ref_count();
+    auto preprocessed_input_trajectory_stream =
+            input_trajectory_stream
+                    .filter(has_min_num_points(2))
+                    .map(transform);
+    auto preprocessed_pattern_trajectory_stream =
+            pattern_trajectory_stream
+                    .filter(has_min_num_points(2))
+                    .map(transform);
+    const trajecmp::distance::neighbours_percentage_range neighbours(0.1);
+    const auto modified_hausdorff = trajecmp::distance::modified_hausdorff(neighbours);
+    auto distance_stream =
+            preprocessed_input_trajectory_stream
+                    .with_latest_from(modified_hausdorff, preprocessed_pattern_trajectory_stream);
+    auto input_matches_pattern_stream = distance_stream.filter(less_than(25));
+    input_matches_pattern_stream
+            .subscribe([](double distance) {
+                std::cout << "input matches pattern with distance " << distance << '\n';
+            });
 
-    // filter to last string in each line
-    auto closes = strings |
-                  filter(
-                          [](const string& s){
-                              return s.back() == '\r';
-                          }) |
-                  Rx::map([](const string&){return 0;});
 
-    // group strings by line
-    auto linewindows = strings |
-                       window_toggle(closes | start_with(0), [=](int){return closes;});
-
-    // reduce the strings for a line into one string
-    auto lines = linewindows |
-                 flat_map([&](observable<string> w) {
-                     return w | start_with<string>("") | sum() | Rx::map(removespaces);
-                 });
-
-    // print result
-    lines |
-    subscribe<string>(println(cout));
-
-    return 0;
+    auto subscriber = input_trajectory_subject.get_subscriber();
+    subscriber.on_next(Trajectory {
+            {128, 405},
+            {130, 397},
+            {130, 381},
+            {125, 331},
+            {123, 275},
+            {117, 235},
+            {120, 233},
+            {161, 238},
+            {185, 238},
+            {192, 232},
+            {199, 230},
+            {197, 232},
+    });
+    subscriber.on_completed();
 }
