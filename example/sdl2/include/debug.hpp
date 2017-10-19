ea7fb126c972c29d1e84e629e99fb67fb65b6b76
Michael Maier <maiermic@cip.ifi.lmu.de>
2017-10-19 22:41:29 +0200

    match input trajectory with pattern letter L

---
+++ example/sdl2/include/debug.hpp
@@ -0,0 +1,21 @@
+#ifndef DEBUG_HPP
+#define DEBUG_HPP
+
+#include <string>
+#include <rxcpp/rx.hpp>
+
+#include "../../logging.hpp"
+#include "model.hpp"
+
+auto debug(std::string description) {
+    return [=](auto &&observable) {
+        using Value = typename rxcpp::util::value_type_from<decltype(observable)>::type;
+        return observable.tap([=](Value value) {
+            logging::is_logging = true;
+            LOG_TEXT(description << ": " << value);
+            logging::is_logging = false;
+        });
+    };
+}
+
+#endif //DEBUG_HPP
--- example/sdl2/src/sdl2-line-example.cpp
+++ example/sdl2/src/sdl2-line-example.cpp
@@ -4,20 +4,67 @@
 #include <color.hpp>
 #include <draw.hpp>
 #include <model.hpp>
+#include <pattern_matching.hpp>
 #include <sdl2_framework.hpp>
 #include <boost/geometry/algorithms/append.hpp>
+#include <boost/geometry/extensions/strategies/cartesian/distance_info.hpp>
+#include "trajecmp/util/boost_geometry_to_string.hpp"
+#include "trajecmp/util/subscribe_with_latest_from.hpp"
+#include "../../logging.hpp"
 
 class framework : public sdl2_framework {
     bool _is_rerender = true;
     bool _is_recording_trajectory = false;
     model::trajectory _trajectory;
 public:
+    framework() {
+        subscribe_to_pattern_matching();
+    }
+
+    void subscribe_to_pattern_matching() {
+        using trajecmp::util::subscribe_with_latest_from;
+        namespace pm = pattern_matching;
+        namespace bg = boost::geometry;
+
+        pm::distance_stream
+        | subscribe_with_latest_from(
+                [&](bg::distance_info_result<model::point> distance,
+                    model::trajectory input_trajectory,
+                    model::trajectory pattern_trajectory) {
+                    static const auto visualization_size = 300;
+                    const auto transform_for_visualization = trajecmp::functional::pipe(
+                            trajecmp::transform::scale_to_const<visualization_size>(pm::normalized_size),
+                            trajecmp::transform::translate_by(model::vector(visualization_size / 2, visualization_size / 2))
+                    );
+                    const auto is_similar = distance.real_distance < pattern_matching::normalized_size * 0.20;
+                    draw_trajectory(_renderer, transform_for_visualization(pattern_trajectory), color_code::yellow);
+                    draw_trajectory(_renderer,
+                                    transform_for_visualization(input_trajectory),
+                                    is_similar ? color_code::green : color_code::red);
+                    model::trajectory distance_trajectory {
+                            distance.projected_point1,
+                            distance.projected_point2,
+                    };
+                    draw_trajectory(_renderer, transform_for_visualization(distance_trajectory), color_code::pink);
+                    std::cout << "distance: " << distance.real_distance << '\n';
+                    _is_rerender = false;
+                },
+                pm::preprocessed_input_trajectory_stream,
+                pm::preprocessed_pattern_trajectory_stream
+        );
+
+        pm::pattern_trajectory_subject
+                .get_subscriber()
+                .on_next(pattern::letter_L);
+    }
+
     void display() override {
+        if (!_is_rerender) {
+            return;
+        }
         SDL_SetRenderDrawColor(_renderer, 0, 0, 0, 255);
         SDL_RenderClear(_renderer);
 
-        circleColor(_renderer, 300, 300, 50, 0xFF00FFFF);
-        draw_line(_renderer, {100, 100}, {200, 200}, color_code::green);
         draw_trajectory(_renderer, _trajectory);
 
         SDL_RenderPresent(_renderer);
@@ -41,6 +88,12 @@ public:
             case SDL_MOUSEBUTTONUP:
                 _is_rerender = false;
                 _is_recording_trajectory = false;
+
+                std::cout << "emit trajectory" << _trajectory << '\n';
+                pattern_matching::input_trajectory_subject
+                        .get_subscriber()
+                        .on_next(_trajectory);
+
                 bg::clear(_trajectory);
                 break;
         }
