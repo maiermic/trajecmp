0c7154060b2ca3e4daa4fe3a1df9b12f81df22eb
Michael Maier <maiermic@cip.ifi.lmu.de>
2017-10-19 21:42:02 +0200

    draw trajectory while mouse button is pressed

--- example/sdl2/src/sdl2-line-example.cpp
+++ example/sdl2/src/sdl2-line-example.cpp
@@ -5,8 +5,12 @@
 #include <draw.hpp>
 #include <model.hpp>
 #include <sdl2_framework.hpp>
+#include <boost/geometry/algorithms/append.hpp>
 
 class framework : public sdl2_framework {
+    bool _is_rerender = true;
+    bool _is_recording_trajectory = false;
+    model::trajectory _trajectory;
 public:
     void display() override {
         SDL_SetRenderDrawColor(_renderer, 0, 0, 0, 255);
@@ -14,8 +18,32 @@ public:
 
         circleColor(_renderer, 300, 300, 50, 0xFF00FFFF);
         draw_line(_renderer, {100, 100}, {200, 200}, color_code::green);
+        draw_trajectory(_renderer, _trajectory);
 
         SDL_RenderPresent(_renderer);
+    }
+
+    void handle_event(const SDL_Event &event) override {
+        namespace bg = boost::geometry;
+        sdl2_framework::handle_event(event);
+        switch (event.type) {
+            case SDL_MOUSEBUTTONDOWN:
+                _is_rerender = true;
+                _is_recording_trajectory = true;
+                bg::append(_trajectory, model::point(event.motion.x, event.motion.y));
+                break;
+            case SDL_MOUSEMOTION:
+                if (_is_recording_trajectory) {
+                    _is_rerender = true;
+                    bg::append(_trajectory, model::point(event.motion.x, event.motion.y));
+                }
+                break;
+            case SDL_MOUSEBUTTONUP:
+                _is_rerender = false;
+                _is_recording_trajectory = false;
+                bg::clear(_trajectory);
+                break;
+        }
     }
 };
 
