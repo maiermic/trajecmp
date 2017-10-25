66da8641ff0c0b6e1c29e8151a3adc58b30bb725
Michael Maier <maiermic@cip.ifi.lmu.de>
2017-10-25 21:05:00 +0200

    add type trait to remove qualifiers of type

---
+++ src/trajecmp/util/type_traits.hpp
@@ -0,0 +1,13 @@
+#ifndef TRAJECMP_UTIL_TYPE_TRAITS_HPP
+#define TRAJECMP_UTIL_TYPE_TRAITS_HPP
+
+namespace trajecmp { namespace util {
+
+    template<typename T>
+    struct remove_cv_ref {
+        using type = typename std::remove_cv<typename std::remove_reference<T>::type>::type;
+    };
+
+}}
+
+#endif //TRAJECMP_UTIL_TYPE_TRAITS_HPP
