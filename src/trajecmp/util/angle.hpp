ef2e152646d57ed79c32a67aaa5a1904778656a9
Michael Maier <maiermic@cip.ifi.lmu.de>
2017-11-02 11:01:32 +0100

    calculate angle between vectors taking into account the direction of rotation

--- src/trajecmp/util/angle.hpp
+++ src/trajecmp/util/angle.hpp
@@ -48,6 +48,75 @@ namespace trajecmp { namespace util {
         }
     }
 
+    /**
+     * Calculates the smallest angle in radians between two vectors taking into
+     * account the direction of rotation.
+     * @tparam Vector The type of the vectors.
+     * @param v1 The start vector.
+     * @param v2 The end vector.
+     * @return Value between [-π, π], which corresponds to the interval
+     * of degree values [-180, 180].
+     * If the smallest angle between the two vectors is clockwise, the angle is
+     * a positive value.
+     * If the smallest angle between the two vectors is counterclockwise, the
+     * angle is a negative value.
+     * If one of the vectors is zero or if the vectors are equal, the angle is
+     * zero.
+     */
+    template<typename Vector>
+    typename boost::geometry::coordinate_type<Vector>::type
+    angle(const Vector &v1, const Vector &v2) {
+        namespace bg = boost::geometry;
+        using trajecmp::trait::number_type_trait;
+        using trajecmp::geometry::point::equals_approx_zero;
+        using Value = typename bg::coordinate_type<Vector>::type;
+        const auto zero = number_type_trait<Value>::get_zero_element();
+        if (equals_approx_zero(v1) || equals_approx_zero(v2)) {
+            return zero;
+        }
+        return std::atan2(bg::get<1>(v1), bg::get<0>(v1)) -
+               std::atan2(bg::get<1>(v2), bg::get<0>(v2));
+    }
+
+    /**
+     * Calculates the smallest clockwise angle in radians between two vectors.
+     * @tparam Vector The type of the vectors.
+     * @param v1 The start vector.
+     * @param v2 The end vector.
+     * @return Value between [0, 2π), which corresponds to the interval
+     * of degree values [0, 360).
+     * If one of the vectors is zero or if the vectors are equal, the angle is
+     * zero.
+     */
+    template<typename Vector>
+    typename boost::geometry::coordinate_type<Vector>::type
+    angle_clockwise(const Vector &v1, const Vector &v2) {
+        namespace bg = boost::geometry;
+        using Value = typename bg::coordinate_type<Vector>::type;
+        const auto a = angle(v1, v2);
+        return a < 0 ? d2r<Value>(Value(360)) + a : a;
+    }
+
+    /**
+     * Calculates the smallest counterclockwise angle in radians between two
+     * vectors.
+     * @tparam Vector The type of the vectors.
+     * @param v1 The start vector.
+     * @param v2 The end vector.
+     * @return Value between [0, 2π), which corresponds to the interval
+     * of degree values [0, 360).
+     * If one of the vectors is zero or if the vectors are equal, the angle is
+     * zero.
+     */
+    template<typename Vector>
+    typename boost::geometry::coordinate_type<Vector>::type
+    angle_counterclockwise(const Vector &v1, const Vector &v2) {
+        namespace bg = boost::geometry;
+        using Value = typename bg::coordinate_type<Vector>::type;
+        const auto a = angle(v1, v2);
+        return a <= 0 ? std::abs(a) : d2r<Value>(Value(360)) - a;
+    }
+
 }}
 
 #endif //TRAJECMP_ANGLE_HPP
--- test/trajecmp/util/angle_test.cpp
+++ test/trajecmp/util/angle_test.cpp
@@ -55,3 +55,118 @@ TEST_CASE("trajecmp::util::enclosed_angle", "[]") {
         }
     }
 }
+
+TEST_CASE("trajecmp::util::angle", "[]") {
+    using trajecmp::model::point2d;
+    const point2d zero{0.0, 0.0};
+    const point2d x_axis{1.0, 0.0};
+    const point2d y_axis{0.0, 1.0};
+    SECTION("is zero if at least one vector is zero") {
+        CHECK(angle(zero, y_axis) == 0.0);
+        CHECK(angle(x_axis, zero) == 0.0);
+        CHECK(angle(zero, zero) == 0.0);
+    }
+    SECTION("is zero if vectors are equal") {
+        CHECK(angle(x_axis, x_axis) == 0.0);
+        CHECK(angle(y_axis, y_axis) == 0.0);
+    }
+    SECTION("45 degree") {
+        CHECK(angle(y_axis, point2d(1.0, 1.0)) ==
+              Approx(d2r(45.0)));
+        // negative angles
+        CHECK(angle(point2d(1.0, 1.0), y_axis) ==
+              Approx(d2r(-45.0)));
+    }
+    SECTION("90 degree") {
+        CHECK(angle(y_axis, x_axis) == d2r(90.0));
+        CHECK(angle(x_axis, point2d(0.0, -1.0)) == d2r(90.0));
+        // negative angles
+        CHECK(angle(x_axis, y_axis) == d2r(-90.0));
+        CHECK(angle(point2d(0.0, -1.0), x_axis) == d2r(-90.0));
+    }
+    SECTION("180 degree") {
+        CHECK(angle(point2d(-1.0, 0.0), x_axis) == d2r(180.0));
+        CHECK(angle(y_axis, point2d(0.0, -1.0)) == d2r(180.0));
+        CHECK(angle(point2d(-1.0, 1.0), point2d(1.0, -1.0)) ==
+              d2r(180.0));
+        CHECK(angle(point2d(1.0, 1.0), point2d(-1.0, -1.0)) ==
+              d2r(180.0));
+        // negative angles
+        CHECK(angle(x_axis, point2d(-1.0, 0.0)) == d2r(-180.0));
+        CHECK(angle(point2d(0.0, -1.0), y_axis) == d2r(-180.0));
+        CHECK(angle(point2d(1.0, -1.0), point2d(-1.0, 1.0)) ==
+              d2r(-180.0));
+        CHECK(angle(point2d(-1.0, -1.0), point2d(1.0, 1.0)) ==
+              d2r(-180.0));
+    }
+}
+
+TEST_CASE("trajecmp::util::angle_clockwise", "[]") {
+    using trajecmp::model::point2d;
+    const point2d zero{0.0, 0.0};
+    const point2d x_axis{1.0, 0.0};
+    const point2d y_axis{0.0, 1.0};
+    SECTION("is zero if at least one vector is zero") {
+        CHECK(angle_clockwise(zero, y_axis) == 0.0);
+        CHECK(angle_clockwise(x_axis, zero) == 0.0);
+        CHECK(angle_clockwise(zero, zero) == 0.0);
+    }
+    SECTION("is zero if vectors are equal") {
+        CHECK(angle(x_axis, x_axis) == 0.0);
+        CHECK(angle(y_axis, y_axis) == 0.0);
+    }
+    SECTION("45 degree") {
+        CHECK(angle_clockwise(y_axis, point2d(1.0, 1.0)) ==
+              Approx(d2r(45.0)));
+    }
+    SECTION("90 degree") {
+        CHECK(angle_clockwise(y_axis, x_axis) == d2r(90.0));
+        CHECK(angle_clockwise(x_axis, point2d(0.0, -1.0)) == d2r(90.0));
+    }
+    SECTION("180 degree") {
+        CHECK(angle_clockwise(x_axis, point2d(-1.0, 0.0)) == d2r(180.0));
+        CHECK(angle_clockwise(y_axis, point2d(0.0, -1.0)) == d2r(180.0));
+        CHECK(angle_clockwise(point2d(-1.0, 1.0), point2d(1.0, -1.0)) ==
+              d2r(180.0));
+        CHECK(angle_clockwise(point2d(-1.0, -1.0), point2d(1.0, 1.0)) ==
+              d2r(180.0));
+    }
+    SECTION("270 degree") {
+        CHECK(angle_clockwise(x_axis, y_axis) == d2r(270.0));
+    }
+}
+
+TEST_CASE("trajecmp::util::angle_counterclockwise", "[]") {
+    using trajecmp::model::point2d;
+    const point2d zero{0.0, 0.0};
+    const point2d x_axis{1.0, 0.0};
+    const point2d y_axis{0.0, 1.0};
+    SECTION("is zero if at least one vector is zero") {
+        CHECK(angle_counterclockwise(zero, y_axis) == 0.0);
+        CHECK(angle_counterclockwise(x_axis, zero) == 0.0);
+        CHECK(angle_counterclockwise(zero, zero) == 0.0);
+    }
+    SECTION("is zero if vectors are equal") {
+        CHECK(angle(x_axis, x_axis) == 0.0);
+        CHECK(angle(y_axis, y_axis) == 0.0);
+    }
+    SECTION("45 degree") {
+        CHECK(angle_counterclockwise(point2d(1.0, 1.0), y_axis) ==
+              Approx(d2r(45.0)));
+    }
+    SECTION("90 degree") {
+        CHECK(angle_counterclockwise(x_axis, y_axis) == d2r(90.0));
+        CHECK(angle_counterclockwise(point2d(0.0, -1.0), x_axis) == d2r(90.0));
+    }
+    SECTION("180 degree") {
+        CHECK(angle_counterclockwise(point2d(-1.0, 0.0), x_axis) == d2r(180.0));
+        CHECK(angle_counterclockwise(point2d(0.0, -1.0), y_axis) == d2r(180.0));
+        CHECK(angle_counterclockwise(point2d(1.0, -1.0), point2d(-1.0, 1.0)) ==
+              d2r(180.0));
+        CHECK(angle_counterclockwise(point2d(1.0, 1.0), point2d(-1.0, -1.0)) ==
+              d2r(180.0));
+    }
+    SECTION("270 degree") {
+        CHECK(angle_counterclockwise(y_axis, x_axis) == d2r(270.0));
+    }
+}
