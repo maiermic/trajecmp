f9785a96ae5d9fb86e64c649ed2b3595df01ace5
Michael Maier <maiermic@cip.ifi.lmu.de>
2017-11-04 08:36:09 +0100

    add subtract vector operator

--- example/sdl2/include/model.hpp
+++ example/sdl2/include/model.hpp
@@ -7,6 +7,13 @@ namespace model {
     using point = boost::geometry::model::point<float, 2, boost::geometry::cs::cartesian>;
     using trajectory = boost::geometry::model::linestring<point>;
     using vector = point;
+
+    vector operator-(const vector lhs, const vector rhs) {
+        vector result(lhs);
+        boost::geometry::subtract_point(result, rhs);
+        return result;
+    }
+
 } // namespace model
 
 #endif //MODEL_HPP
