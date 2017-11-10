c3707067a7d7ffdda624c602f70c7ca987ef66b4
Michael Maier <maiermic@cip.ifi.lmu.de>
2017-11-10 13:09:11 +0100

    use range to iterate angle interval

--- test/trajecmp/gesture/circle_test.cpp
+++ test/trajecmp/gesture/circle_test.cpp
@@ -1,6 +1,7 @@
 #include <trajecmp/gesture/circle.hpp>
 #include <trajecmp/trajectory/circle.hpp>
 #include <trajecmp/model/trajectory.hpp>
+#include <trajecmp/range/range.hpp>
 
 #include <catch.hpp>
 #include <trajecmp/geometry/min_bounding_sphere.hpp>
@@ -8,6 +9,7 @@
 using trajecmp::util::r2d;
 using trajecmp::model::trajectory2d;
 using trajecmp::model::point2d;
+using trajecmp::range::range;
 using circle_trajectory = trajecmp::trajectory::circle<trajectory2d>;
 using Angle = typename boost::geometry::coordinate_type<trajectory2d>::type;
 
@@ -41,17 +43,17 @@ void check_circle_segment(Angle start_angle,
 
 TEST_CASE("trajecmp::gesture::estimate_circle_segment", "[]") {
     SECTION("radius of 100, start angle of 0 and end angle between -1080 and 1080 with step size of 10") {
-        for (double end_angle = -1080.0; end_angle <= 1080.0; end_angle += 10.0) {
+        for (double end_angle : range(-1080.0, 1080.0, 10.0)) {
             check_circle_segment(0.0, end_angle, 100.0);
         }
     }
     SECTION("radius of 1000000, start angle of 0 and end angle between -1080 and 1080 with step size of 10") {
-        for (double end_angle = -1080.0; end_angle <= 1080.0; end_angle += 10.0) {
+        for (double end_angle : range(-1080.0, 1080.0, 10.0)) {
             check_circle_segment(0.0, end_angle, 1000000.0);
         }
     }
     SECTION("radius of 0.000001, start angle of 0 and end angle between -1080 and 1080 with step size of 10") {
-        for (double end_angle = -1080.0; end_angle <= 1080.0; end_angle += 10.0) {
+        for (double end_angle : range(-1080.0, 1080.0, 10.0)) {
             check_circle_segment(0.0, end_angle, 0.000001);
         }
     }
