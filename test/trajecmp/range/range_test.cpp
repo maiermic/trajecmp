25fbd259f043f17d6ae0bab251371bd936e1d94f
Michael Maier <maiermic@cip.ifi.lmu.de>
2017-11-10 16:36:51 +0100

    test range function in single test case

--- test/trajecmp/range/range_test.cpp
+++ test/trajecmp/range/range_test.cpp
@@ -15,74 +15,71 @@ void check_range(std::initializer_list<Value> expected, Range actual) {
           std::vector<Value>(boost::begin(actual), boost::end(actual)));
 };
 
-TEST_CASE("range (int)", "[range]") {
-    SECTION("range step default") {
+TEST_CASE("trajecmp::range::range", "[range]") {
+    // int
+    SECTION("range step default (int)") {
         check_range({0, 1, 2}, range(0, 2));
     }
-    SECTION("range increasing step 1") {
+    SECTION("range increasing step 1 (int)") {
         check_range({0, 1, 2}, range(0, 2, 1));
     }
-    SECTION("range increasing step 2") {
+    SECTION("range increasing step 2 (int)") {
         check_range({0, 2}, range(0, 2, 2));
     }
-    SECTION("range decreasing step 1") {
+    SECTION("range decreasing step 1 (int)") {
         check_range({0, -1, -2}, range(0, -2, 1));
     }
-    SECTION("range decreasing step 2") {
+    SECTION("range decreasing step 2 (int)") {
         check_range({0, -2}, range(0, -2, 2));
     }
-    SECTION("range decreasing step -1") {
+    SECTION("range decreasing step -1 (int)") {
         check_range({0, -1, -2}, range(0, -2, -1));
     }
-    SECTION("range step 0 throws exception") {
+    SECTION("range step 0 throws exception (int)") {
         CHECK_THROWS(range(0, 2, 0));
     }
-}
-
-TEST_CASE("range (float)", "[range]") {
-    SECTION("range step default") {
+    // float
+    SECTION("range step default (float)") {
         check_range({0.0f, 1.0f, 2.0f}, range(0.0f, 2.0f));
     }
-    SECTION("range increasing step 1.0f") {
+    SECTION("range increasing step 1.0f (float)") {
         check_range({0.0f, 1.0f, 2.0f}, range(0.0f, 2.0f, 1.0f));
     }
-    SECTION("range increasing step 2.0f") {
+    SECTION("range increasing step 2.0f (float)") {
         check_range({0.0f, 2.0f}, range(0.0f, 2.0f, 2.0f));
     }
-    SECTION("range decreasing step 1") {
+    SECTION("range decreasing step 1 (float)") {
         check_range({0.0f, -1.0f, -2.0f}, range(0.0f, -2.0f, 1.0f));
     }
-    SECTION("range decreasing step 2.0f") {
+    SECTION("range decreasing step 2.0f (float)") {
         check_range({0.0f, -2.0f}, range(0.0f, -2.0f, 2.0f));
     }
-    SECTION("range decreasing step -1.0f") {
+    SECTION("range decreasing step -1.0f (float)") {
         check_range({0.0f, -1.0f, -2.0f}, range(0.0f, -2.0f, -1.0f));
     }
-    SECTION("range step 0 throws exception") {
+    SECTION("range step 0 throws exception (float)") {
         CHECK_THROWS(range(0.0f, 2.0f, 0.0f));
     }
-}
-
-TEST_CASE("range (double)", "[range]") {
-    SECTION("range step default") {
+    // double
+    SECTION("range step default (double)") {
         check_range({0.0, 1.0, 2.0}, range(0.0, 2.0));
     }
-    SECTION("range increasing step 1.0") {
+    SECTION("range increasing step 1.0 (double)") {
         check_range({0.0, 1.0, 2.0}, range(0.0, 2.0, 1.0));
     }
-    SECTION("range increasing step 2.0") {
+    SECTION("range increasing step 2.0 (double)") {
         check_range({0.0, 2.0}, range(0.0, 2.0, 2.0));
     }
-    SECTION("range decreasing step 1") {
+    SECTION("range decreasing step 1 (double)") {
         check_range({0.0, -1.0, -2.0}, range(0.0, -2.0, 1.0));
     }
-    SECTION("range decreasing step 2.0") {
+    SECTION("range decreasing step 2.0 (double)") {
         check_range({0.0, -2.0}, range(0.0, -2.0, 2.0));
     }
-    SECTION("range decreasing step -1.0") {
+    SECTION("range decreasing step -1.0 (double)") {
         check_range({0.0, -1.0, -2.0}, range(0.0, -2.0, -1.0));
     }
-    SECTION("range step 0 throws exception") {
+    SECTION("range step 0 throws exception (double)") {
         CHECK_THROWS(range(0.0, 2.0, 0.0));
     }
 }
