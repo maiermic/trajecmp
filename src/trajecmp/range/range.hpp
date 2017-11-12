5c3401ff0a649b68f3e68268ef707869b8b6b000
Michael Maier <maiermic@cip.ifi.lmu.de>
2017-11-12 12:22:03 +0100

    add range_bound_always_included

--- src/trajecmp/range/range.hpp
+++ src/trajecmp/range/range.hpp
@@ -3,8 +3,12 @@
 
 #include <cmath>
 
+#include <range/v3/view/any_view.hpp>
+#include <range/v3/view/concat.hpp>
 #include <range/v3/view/iota.hpp>
+#include <range/v3/view/single.hpp>
 #include <range/v3/view/transform.hpp>
+#include <range/v3/view/unique.hpp>
 
 namespace trajecmp { namespace range {
 
@@ -48,6 +52,25 @@ namespace trajecmp { namespace range {
     template <class Value>
     auto range(Value from, Value to) {
         return range(from, to, Value(1));
+    }
+
+    /**
+     * Range that iterates from one value to another using a specific step size
+     * and always including the end value even if it is overstepped.
+     *
+     * @tparam Value The type of the values to iterate.
+     * @param from Start value (inclusive).
+     * @param to End value (always included).
+     * @param step_size The increment value.
+     * @return Range of values.
+     */
+    template <class Value>
+    auto range_bound_always_included(Value from, Value to, Value step_size = 1) {
+        using ranges::view::concat;
+        using ranges::view::single;
+        using ranges::view::unique;
+        using ranges::any_view;
+        return unique(concat(range(from, to, step_size), single(to)));
     }
 
 }}
--- test/trajecmp/range/range_test.cpp
+++ test/trajecmp/range/range_test.cpp
@@ -1,18 +1,16 @@
 #include <vector>
+#include "trajecmp/range/range.hpp"
+#include <range/v3/to_container.hpp>
 
 #include <catch.hpp>
-
-#include <boost/range.hpp>
-
-#include "trajecmp/range/range.hpp"
 
 using namespace trajecmp::range;
 
 
-template <class Range, class Value = boost::iterator_value<Range>>
+template<class Range, class Value>
 void check_range(std::initializer_list<Value> expected, Range actual) {
     CHECK((std::vector<Value>(expected)) ==
-          std::vector<Value>(boost::begin(actual), boost::end(actual)));
+          ranges::to_vector(actual));
 };
 
 TEST_CASE("trajecmp::range::range", "[range]") {
@@ -81,5 +79,104 @@ TEST_CASE("trajecmp::range::range", "[range]") {
     }
     SECTION("range step 0 throws exception (double)") {
         CHECK_THROWS(range(0.0, 2.0, 0.0));
+    }
+}
+
+TEST_CASE("trajecmp::range::range_bound_always_included", "[]") {
+    // int
+    SECTION("range step default (int)") {
+        check_range({0, 1, 2}, range_bound_always_included(0, 2));
+    }
+    SECTION("range increasing step 1 (int)") {
+        check_range({0, 1, 2}, range_bound_always_included(0, 2, 1));
+    }
+    SECTION("range increasing step 2 (int)") {
+        check_range({0, 2}, range_bound_always_included(0, 2, 2));
+    }
+    SECTION("range decreasing step 1 (int)") {
+        check_range({0, -1, -2}, range_bound_always_included(0, -2, 1));
+    }
+    SECTION("range decreasing step 2 (int)") {
+        check_range({0, -2}, range_bound_always_included(0, -2, 2));
+    }
+    SECTION("range decreasing step -1 (int)") {
+        check_range({0, -1, -2}, range_bound_always_included(0, -2, -1));
+    }
+    SECTION("range step 0 throws exception (int)") {
+        CHECK_THROWS(range(0, 2, 0));
+    }
+    SECTION("range contains bound even if overstepped (int)") {
+        check_range({0, 2, 3},
+                    range_bound_always_included(0, 3, 2));
+        check_range({0, -2, -3},
+                    range_bound_always_included(0, -3, 2));
+    }
+    // float
+    SECTION("range step default (float)") {
+        check_range({0.0f, 1.0f, 2.0f}, range_bound_always_included(0.0f, 2.0f));
+    }
+    SECTION("range increasing step 1.0f (float)") {
+        check_range({0.0f, 1.0f, 2.0f}, range_bound_always_included(0.0f, 2.0f, 1.0f));
+    }
+    SECTION("range increasing step 2.0f (float)") {
+        check_range({0.0f, 2.0f}, range_bound_always_included(0.0f, 2.0f, 2.0f));
+    }
+    SECTION("range decreasing step 1 (float)") {
+        check_range({0.0f, -1.0f, -2.0f}, range_bound_always_included(0.0f, -2.0f, 1.0f));
+    }
+    SECTION("range decreasing step 2.0f (float)") {
+        check_range({0.0f, -2.0f}, range_bound_always_included(0.0f, -2.0f, 2.0f));
+    }
+    SECTION("range decreasing step -1.0f (float)") {
+        check_range({0.0f, -1.0f, -2.0f}, range_bound_always_included(0.0f, -2.0f, -1.0f));
+    }
+    SECTION("range step 0 throws exception (float)") {
+        CHECK_THROWS(range(0.0f, 2.0f, 0.0f));
+    }
+    SECTION("range contains bound even if overstepped (float)") {
+        check_range({0.0f, 1.0f, 2.0f, 2.1f},
+                    range_bound_always_included(0.0f, 2.1f, 1.0f));
+        check_range({0.0f, 1.0f, 2.0f, 2.5f},
+                    range_bound_always_included(0.0f, 2.5f, 1.0f));
+        check_range({0.0f, 2.0f, 3.0f},
+                    range_bound_always_included(0.0f, 3.0f, 2.0f));
+        check_range({0.0f, -1.0f, -2.0f, -2.1f},
+                    range_bound_always_included(0.0f, -2.1f, 1.0f));
+        check_range({0.0f, -2.0f, -2.1f},
+                    range_bound_always_included(0.0f, -2.1f, 2.0f));
+        check_range({0.0f, -1.0f, -2.0f, -2.1f},
+                    range_bound_always_included(0.0f, -2.1f, -1.0f));
+    }
+    // double
+    SECTION("range step default (double)") {
+        check_range({0.0, 1.0, 2.0}, range_bound_always_included(0.0, 2.0));
+    }
+    SECTION("range increasing step 1.0 (double)") {
+        check_range({0.0, 1.0, 2.0}, range_bound_always_included(0.0, 2.0, 1.0));
+    }
+    SECTION("range increasing step 2.0 (double)") {
+        check_range({0.0, 2.0}, range_bound_always_included(0.0, 2.0, 2.0));
+    }
+    SECTION("range decreasing step 1 (double)") {
+        check_range({0.0, -1.0, -2.0}, range_bound_always_included(0.0, -2.0, 1.0));
+    }
+    SECTION("range decreasing step 2.0 (double)") {
+        check_range({0.0, -2.0}, range_bound_always_included(0.0, -2.0, 2.0));
+    }
+    SECTION("range decreasing step -1.0 (double)") {
+        check_range({0.0, -1.0, -2.0}, range_bound_always_included(0.0, -2.0, -1.0));
+    }
+    SECTION("range step 0 throws exception (double)") {
+        CHECK_THROWS(range(0.0, 2.0, 0.0));
+    }
+    SECTION("range contains bound even if overstepped (double)") {
+        check_range({0.0, 1.0, 2.0, 2.1}, range_bound_always_included(0.0, 2.1, 1.0));
+        check_range({0.0, 1.0, 2.0, 2.5}, range_bound_always_included(0.0, 2.5, 1.0));
+        check_range({0.0, 2.0, 3.0}, range_bound_always_included(0.0, 3.0, 2.0));
+        check_range({0.0, -1.0, -2.0, -2.1},
+                    range_bound_always_included(0.0, -2.1, 1.0));
+        check_range({0.0, -2.0, -2.1}, range_bound_always_included(0.0, -2.1, 2.0));
+        check_range({0.0, -1.0, -2.0, -2.1},
+                    range_bound_always_included(0.0, -2.1, -1.0));
     }
 }
