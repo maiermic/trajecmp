00ef52a3ac011b3a8014988d2a480261826bbe37
Michael Maier <maiermic@cip.ifi.lmu.de>
2017-11-21 09:00:11 +0100

    estimate circle segment (used as pattern) based on input trajectory

--- example/sdl2/src/sdl2-count-circles.cpp
+++ example/sdl2/src/sdl2-count-circles.cpp
@@ -18,7 +18,51 @@
 #include "trajecmp/util/find_local_extrema.hpp"
 #include "trajecmp/util/subscribe_with_latest_from.hpp"
 #include "trajecmp/util/vector_ostream.hpp"
+#include "trajecmp/gesture/circle.hpp"
+#include "trajecmp/trajectory/circle.hpp"
+#include "trajecmp/transform/douglas_peucker.hpp"
 #include "../../logging.hpp"
+
+struct circle_comparison_data {
+    model::trajectory preprocessed_input_trajectory;
+    model::trajectory preprocessed_pattern_trajectory;
+    boost::geometry::distance_info_result<model::point> distance;
+};
+
+circle_comparison_data
+get_circle_comparison_data(const model::trajectory &input_trajectory) {
+    using trajecmp::util::r2d;
+    namespace pm = pattern_matching;
+    const auto mbs = trajecmp::geometry::min_bounding_sphere(input_trajectory);
+    const auto c = trajecmp::gesture::estimate_circle_segment(input_trajectory, mbs);
+    LOG(r2d(c.start_angle));
+    LOG(r2d(c.end_angle));
+    LOG_SEP();
+    const auto pattern_trajectory =
+            trajecmp::trajectory::circle<model::trajectory>(c.radius)
+                    .sample(r2d(c.start_angle), r2d(c.end_angle), 5.0f);
+    const auto preprocess_input = [&](model::trajectory trajectory) {
+        return trajecmp::transform::scale_to_const<pm::normalized_size>(mbs.radius * 2)(
+                trajecmp::transform::translate_by(trajecmp::geometry::negative_vector_of(mbs.center))(
+                        trajectory)
+        );
+    };
+    const auto preprocess_pattern = [&](model::trajectory trajectory) {
+        return trajecmp::transform::scale_to_const<pm::normalized_size>(mbs.radius * 2)(trajectory);
+    };
+    const model::trajectory preprocessed_input_trajectory =
+            preprocess_input(input_trajectory);
+    const model::trajectory preprocessed_pattern_trajectory =
+            preprocess_pattern(pattern_trajectory);
+    return {
+            preprocessed_input_trajectory,
+            preprocessed_pattern_trajectory,
+            pm::modified_hausdorff_info(
+                    preprocessed_input_trajectory,
+                    preprocessed_pattern_trajectory
+            ),
+    };
+}
 
 class framework : public sdl2_framework {
     bool _is_rerender = true;
@@ -34,97 +78,51 @@ public:
         namespace pm = pattern_matching;
         namespace bg = boost::geometry;
 
-        pm::distance_stream
-        .with_latest_from(
-                [&](bg::distance_info_result<model::point> distance,
-                    model::trajectory input_trajectory,
-                    model::trajectory pattern_trajectory) {
+        pm::input_trajectory_stream
+                .map(trajecmp::transform::douglas_peucker(3))
+                .filter(trajecmp::predicate::has_min_num_points(3))
+                .map(get_circle_comparison_data)
+                .subscribe([&](const circle_comparison_data &data) {
+                    const model::trajectory &input_trajectory =
+                            data.preprocessed_input_trajectory;
+                    const model::trajectory &pattern_trajectory =
+                            data.preprocessed_pattern_trajectory;
+                    const bg::distance_info_result<model::point> &distance =
+                            data.distance;
                     static const auto visualization_size = 300;
                     static const auto center_x = visualization_size / 2;
                     static const auto center_y = visualization_size / 2;
                     const model::vector center(center_x, center_y);
                     const auto transform_for_visualization = trajecmp::functional::pipe(
-                            trajecmp::transform::scale_to_const<visualization_size>(pm::normalized_size),
+                            trajecmp::transform::scale_to_const<visualization_size>(
+                                    pm::normalized_size),
                             trajecmp::transform::translate_by(center)
                     );
-                    const auto is_similar = distance.real_distance < pattern_matching::normalized_size * 0.20;
-//                    draw_trajectory(_renderer, transform_for_visualization(pattern_trajectory), color_code::yellow);
+                    const auto is_similar = distance.real_distance <
+                                            pattern_matching::normalized_size *
+                                            0.20;
+
+                    LOG(pattern_trajectory);
+                    draw_trajectory(_renderer, transform_for_visualization(
+                            pattern_trajectory), color_code::yellow);
                     const model::trajectory visualization_input_trajectory =
                             transform_for_visualization(input_trajectory);
                     draw_trajectory(_renderer,
                                     visualization_input_trajectory,
-                                    is_similar ? color_code::green : color_code::red);
-                    model::trajectory distance_trajectory {
+                                    is_similar ? color_code::green
+                                               : color_code::red);
+                    model::trajectory distance_trajectory{
                             distance.projected_point1,
                             distance.projected_point2,
                     };
-                    draw_trajectory(_renderer, transform_for_visualization(distance_trajectory), color_code::pink);
+                    draw_trajectory(_renderer, transform_for_visualization(
+                            distance_trajectory), color_code::pink);
                     draw_box(_renderer, center, 10, color_code::gray);
-                    std::cout << "distance: " << distance.real_distance << '\n';
-
-                    std::vector<float> distances_to_start;
-                    const model::point start =
-                            visualization_input_trajectory.front();
-                    std::transform(
-                            std::begin(visualization_input_trajectory),
-                            std::end(visualization_input_trajectory),
-                            std::back_inserter(distances_to_start),
-                            [&](const model::point &current) {
-                                return boost::geometry::distance(start, current);
-                            }
-                    );
-                    const auto extrema =
-                            trajecmp::util::find_local_extrema(distances_to_start, 10.0f); // TODO determine epsilon based on trajectory size (minimum bounding sphere radius
-                    LOG(extrema.maxima);
-                    for (auto i : extrema.maxima) {
-                        LOG(visualization_input_trajectory.at(i));
-                        draw_box(_renderer, visualization_input_trajectory.at(i), 10, color_code::purple);
-                    }
-                    LOG(extrema.minima);
-                    for (auto i : extrema.minima) {
-                        LOG(visualization_input_trajectory.at(i));
-                        draw_box(_renderer, visualization_input_trajectory.at(i), 10, color_code::blue);
-                    }
-                    using trajecmp::util::angle_clockwise;
-                    using trajecmp::util::angle_counterclockwise;
-                    using trajecmp::util::r2d;
-                    using model::operator-;
-                    const model::point x_achsis(1, 0);
-                    const auto start_angle = angle_clockwise(
-                            x_achsis,
-                            visualization_input_trajectory.front() - center
-                    );
-                    LOG(center);
-                    LOG(visualization_input_trajectory.front());
-                    LOG(visualization_input_trajectory.front() - center);
-                    const auto end_angle = angle_clockwise(
-                            x_achsis,
-                            visualization_input_trajectory.back() - center
-                    );
-                    LOG(visualization_input_trajectory.back() - center);
-                    LOG(r2d(start_angle));
-                    LOG(r2d(end_angle));
-                    draw_trajectory(_renderer,
-                                    trajecmp::transform::translate_by(center)(
-                                        trajecmp::trajectory::circle<model::trajectory>(150)
-                                                .sample(r2d(start_angle), r2d(end_angle), 10)
-                                    ),
-                                    color_code::yellow);
+                    LOG(distance.real_distance);
 
                     SDL_RenderPresent(_renderer);
                     _is_rerender = false;
-                    return 0; // dummy value
-                },
-                pm::preprocessed_input_trajectory_stream,
-                pm::preprocessed_pattern_trajectory_stream
-        ).subscribe([](auto _) {});
-
-        pm::pattern_trajectory_subject
-                .get_subscriber()
-                .on_next(
-                        trajecmp::trajectory::circle<model::trajectory>(1)
-                                .sample(0, -360, 10)
-                );
+                });
     }
 
     void display() override {
