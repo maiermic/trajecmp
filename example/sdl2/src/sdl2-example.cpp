48b9131261cfb6bfd53cfc37f87e0681a341044d
Michael Maier <maiermic@cip.ifi.lmu.de>
2018-01-17 21:48:16 +0100

    use record_trajectory_sdl2_framework in app sdl2-example

--- example/sdl2/src/sdl2-example.cpp
+++ example/sdl2/src/sdl2-example.cpp
@@ -8,101 +8,84 @@
 #include <sdl2_framework.hpp>
 #include <boost/geometry/algorithms/append.hpp>
 #include <boost/geometry/extensions/strategies/cartesian/distance_info.hpp>
+#include <record_trajectory_sdl2_framework.hpp>
 #include "trajecmp/util/boost_geometry_to_string.hpp"
 #include "trajecmp/util/subscribe_with_latest_from.hpp"
 #include "../../logging.hpp"
 
-class framework : public sdl2_framework {
-    bool _is_rerender = true;
-    bool _is_recording_trajectory = false;
-    model::trajectory _trajectory;
+void translate_and_scale(model::trajectory &trajectory) {
+    namespace pm = pattern_matching;
+    using trajecmp::transform::translate_by;
+    using trajecmp::geometry::negative_vector_of;
+    using trajecmp::transform::scale_to_const;
+
+    const auto mbs = trajecmp::geometry::min_bounding_sphere(trajectory);
+    trajectory = translate_by(negative_vector_of(mbs.center))(trajectory);
+    trajectory = scale_to_const<pm::normalized_size>(mbs.radius * 2)(trajectory);
+}
+
+class framework : public record_trajectory_sdl2_framework {
+    const model::trajectory _pattern;
+
 public:
-    framework() {
-        subscribe_to_pattern_matching();
+    framework(const model::trajectory &pattern) : _pattern(pattern) {}
+
+    bool preprocess_input_trajectory(model::trajectory &input) {
+        using boost::geometry::num_points;
+
+        input = trajecmp::transform::douglas_peucker(3)(input);
+        if (num_points(input) < num_points(_pattern)) return false;
+        translate_and_scale(input);
+        return true;
     }
 
-    void subscribe_to_pattern_matching() {
-        using trajecmp::util::subscribe_with_latest_from;
-        namespace pm = pattern_matching;
-        namespace bg = boost::geometry;
-
-        pm::distance_stream
-        .with_latest_from(
-                [&](bg::distance_info_result<model::point> distance,
-                    model::trajectory input_trajectory,
-                    model::trajectory pattern_trajectory) {
-                    static const auto visualization_size = 300;
-                    const auto transform_for_visualization = trajecmp::functional::pipe(
-                            trajecmp::transform::scale_to_const<visualization_size>(pm::normalized_size),
-                            trajecmp::transform::translate_by(model::vector(visualization_size / 2, visualization_size / 2))
-                    );
-                    const auto is_similar = distance.real_distance < pattern_matching::normalized_size * 0.20;
-                    draw_trajectory(_renderer, transform_for_visualization(pattern_trajectory), color_code::yellow);
-                    draw_trajectory(_renderer,
-                                    transform_for_visualization(input_trajectory),
-                                    is_similar ? color_code::green : color_code::red);
-                    model::trajectory distance_trajectory {
-                            distance.projected_point1,
-                            distance.projected_point2,
-                    };
-                    draw_trajectory(_renderer, transform_for_visualization(distance_trajectory), color_code::pink);
-                    std::cout << "distance: " << distance.real_distance << '\n';
-                    SDL_RenderPresent(_renderer);
-                    _is_rerender = false;
-                    return 0; // dummy value
-                },
-                pm::preprocessed_input_trajectory_stream,
-                pm::preprocessed_pattern_trajectory_stream
-        ).subscribe([](auto _) {});
-
-        pm::pattern_trajectory_subject
-                .get_subscriber()
-                .on_next(pattern::letter_L);
-    }
-
-    void display() override {
-        if (!_is_rerender) {
-            return;
+    void handle_input_trajectory(model::trajectory input) {
+        if (preprocess_input_trajectory(input)) {
+            draw_comparison_data(
+                    pattern_matching::modified_hausdorff_info(input, _pattern),
+                    input,
+                    _pattern
+            );
         }
-        SDL_SetRenderDrawColor(_renderer, 0, 0, 0, 255);
-        SDL_RenderClear(_renderer);
+    }
 
-        draw_trajectory(_renderer, _trajectory);
-
+    void draw_comparison_data(
+            boost::geometry::distance_info_result <model::point> distance,
+            model::trajectory input_trajectory,
+            model::trajectory pattern_trajectory) {
+        static const auto visualization_size = 300;
+        const auto transform_for_visualization = trajecmp::functional::pipe(
+                trajecmp::transform::scale_to_const<visualization_size>(
+                        pattern_matching::normalized_size),
+                trajecmp::transform::translate_by(
+                        model::vector(visualization_size / 2,
+                                      visualization_size / 2))
+        );
+        const auto is_similar = distance.real_distance <
+                                pattern_matching::normalized_size * 0.20;
+        draw_trajectory(_renderer,
+                        transform_for_visualization(pattern_trajectory),
+                        color_code::yellow);
+        draw_trajectory(_renderer,
+                        transform_for_visualization(input_trajectory),
+                        is_similar ? color_code::green : color_code::red);
+        model::trajectory distance_trajectory{
+                distance.projected_point1,
+                distance.projected_point2,
+        };
+        draw_trajectory(_renderer,
+                        transform_for_visualization(distance_trajectory),
+                        color_code::pink);
+        std::cout << "distance: " << distance.real_distance << '\n';
         SDL_RenderPresent(_renderer);
+        is_rerender(false);
     }
 
-    void handle_event(const SDL_Event &event) override {
-        namespace bg = boost::geometry;
-        sdl2_framework::handle_event(event);
-        switch (event.type) {
-            case SDL_MOUSEBUTTONDOWN:
-                _is_rerender = true;
-                _is_recording_trajectory = true;
-                bg::append(_trajectory, model::point(event.motion.x, event.motion.y));
-                break;
-            case SDL_MOUSEMOTION:
-                if (_is_recording_trajectory) {
-                    _is_rerender = true;
-                    bg::append(_trajectory, model::point(event.motion.x, event.motion.y));
-                }
-                break;
-            case SDL_MOUSEBUTTONUP:
-                _is_rerender = false;
-                _is_recording_trajectory = false;
-
-                std::cout << "emit trajectory" << _trajectory << std::endl;
-                pattern_matching::input_trajectory_subject
-                        .get_subscriber()
-                        .on_next(_trajectory);
-
-                bg::clear(_trajectory);
-                break;
-        }
-    }
 };
 
 int main() {
-    framework().start();
+    model::trajectory pattern = ::pattern::letter_L;
+    translate_and_scale(pattern);
+    framework(pattern).start();
     return 0;
 }
