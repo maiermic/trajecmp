9d3638d9f96d2ff9f88501a4cbe320af6deb5ce9
Michael Maier <maiermic@cip.ifi.lmu.de>
2018-01-20 00:19:14 +0100

    add scale_and_translate

--- src/trajecmp/transform/translate_and_scale.hpp
+++ src/trajecmp/transform/translate_and_scale.hpp
@@ -28,6 +28,24 @@ namespace trajecmp { namespace transform {
         }
     };
 
+    template<
+            typename Trajectory,
+            typename Scale = typename boost::geometry::coordinate_type<Trajectory>::type,
+            typename Vector = typename boost::geometry::point_type<Trajectory>::type
+    >
+    void scale_and_translate(Scale scale,
+                             const Vector &vector,
+                             Trajectory &trajectory) {
+        using Point = Vector;
+        using Coordinate = typename boost::geometry::coordinate_type<Trajectory>::type;
+        for (Point &point : trajectory) {
+            map_coordinates_with_index(point, [&](std::size_t i, Coordinate c) {
+                using trajecmp::geometry::point::get;
+                return c * scale + get(i, vector);
+            });
+        }
+    };
+
 }} // namespace trajecmp::transform
 
 #endif //TRAJECMP_TRANSFORM_TRANSLATE_AND_SCALE_HPP
