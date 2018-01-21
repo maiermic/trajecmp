d14731b9eea6a8aa456752c30b332af573f412b1
Michael Maier <maiermic@cip.ifi.lmu.de>
2018-01-21 15:15:55 +0100

    add translate_and_scale_using_mbs

--- src/trajecmp/transform/translate_and_scale.hpp
+++ src/trajecmp/transform/translate_and_scale.hpp
@@ -1,11 +1,14 @@
 #ifndef TRAJECMP_TRANSFORM_TRANSLATE_AND_SCALE_HPP
 #define TRAJECMP_TRANSFORM_TRANSLATE_AND_SCALE_HPP
 
+#include <boost/geometry/algorithms/assign.hpp>
 #include <boost/geometry/core/coordinate_type.hpp>
 #include <boost/geometry/core/point_type.hpp>
 #include <boost/geometry/core/coordinate_dimension.hpp>
 
 #include <trajecmp/geometry/point/point.hpp>
+#include <trajecmp/geometry/hyper_sphere.hpp>
+#include <trajecmp/geometry/min_bounding_sphere.hpp>
 #include "map_coordinates.hpp"
 
 namespace trajecmp { namespace transform {
@@ -27,6 +30,62 @@ namespace trajecmp { namespace transform {
             });
         }
     };
+
+    /**
+     * Moves the trajectory so that the center of the MBS (minimum bounding
+     * sphere) lies in the origin and scales the trajectory so that the MBS of
+     * the transformed trajectory corresponds to the diameter of the MBS.
+     * Since the MBS is not passed, it is calculated internally.
+     *
+     * @tparam Size
+     * @tparam Trajectory
+     * @param size The diameter of the MBS of the transformed trajectory.
+     * @param trajectory The trajectory that should be transformed.
+     */
+    template<
+            typename Size,
+            typename Trajectory
+    >
+    void translate_and_scale_using_mbs(Size size, Trajectory &trajectory) {
+        const auto mbs = trajecmp::geometry::min_bounding_sphere(trajectory);
+        trajecmp::transform::translate_and_scale(
+                trajecmp::geometry::negative_vector_of(mbs.center),
+                size / (mbs.radius * 2),
+                trajectory
+        );
+    }
+
+    /**
+     * Moves the trajectory so that the center of the MBS (minimum bounding
+     * sphere) lies in the origin and scales the trajectory so that the MBS of
+     * the transformed trajectory corresponds to the diameter of the MBS.
+     * <code>mbs</code> is transformed so that it is the MBS of the transformed
+     * trajectory.
+     *
+     * @tparam Size
+     * @tparam Trajectory
+     * @param size The diameter of the MBS of the transformed trajectory.
+     * @param mbs The minimum bounding sphere of the trajectory before this
+     * transformation. It is also transformed so that the center lies in the
+     * origin and the radius corresponds to <code>size</code>.
+     * @param trajectory The trajectory that should be transformed.
+     */
+    template<
+            typename Size,
+            typename Trajectory
+    >
+    void translate_and_scale_using_mbs(
+            Size size,
+            trajecmp::geometry::hyper_sphere_of<Trajectory> &mbs,
+            Trajectory &trajectory) {
+        trajecmp::transform::translate_and_scale(
+                trajecmp::geometry::negative_vector_of(mbs.center),
+                size / (mbs.radius * 2),
+                trajectory
+        );
+        boost::geometry::assign_zero(mbs.center);
+        mbs.radius = size;
+    }
 
     template<
             typename Trajectory,
