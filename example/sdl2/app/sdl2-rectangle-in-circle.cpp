2b6d3fc6bd93cf149fc2596a83ba1e7d5cba7ffd
Michael Maier <maiermic@cip.ifi.lmu.de>
2018-01-23 23:21:32 +0100

    add app sdl2-rectangle-in-circle

---
+++ example/sdl2/app/sdl2-rectangle-in-circle.cpp
@@ -0,0 +1,156 @@
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL2_gfxPrimitives.h>
+#include <color.hpp>
+#include <draw.hpp>
+#include <model.hpp>
+#include <pattern_matching.hpp>
+#include <sdl2_framework.hpp>
+#include <boost/geometry/algorithms/append.hpp>
+#include <boost/geometry/algorithms/distance.hpp>
+#include <boost/geometry/extensions/strategies/cartesian/distance_info.hpp>
+#include <trajecmp/gesture/rectangle.hpp>
+#include <trajecmp/gesture/circle.hpp>
+#include "trajecmp/geometry/hyper_sphere.hpp"
+#include "trajecmp/util/angle.hpp"
+#include "trajecmp/util/boost_geometry_to_string.hpp"
+#include "trajecmp/trajectory/circle.hpp"
+#include "trajecmp/util/find_local_extrema.hpp"
+#include "trajecmp/util/subscribe_with_latest_from.hpp"
+#include "trajecmp/util/vector_ostream.hpp"
+#include "trajecmp/geometry/point/arithmetic.hpp"
+#include "trajecmp/gesture/circle.hpp"
+#include "trajecmp/gesture/rectangle.hpp"
+#include "trajecmp/trajectory/circle.hpp"
+#include "trajecmp/transform/douglas_peucker.hpp"
+#include "trajecmp/transform/translate_and_scale.hpp"
+#include "../../logging.hpp"
+#include "record_trajectory_sdl2_framework.hpp"
+
+struct circle_comparison_data {
+    model::trajectory preprocessed_input_trajectory;
+    model::trajectory preprocessed_pattern_trajectory;
+    boost::geometry::distance_info_result<model::point> distance;
+};
+
+circle_comparison_data
+get_circle_comparison_data(
+        const model::trajectory &input_trajectory,
+        const trajecmp::geometry::hyper_sphere_of<model::trajectory> &mbs) {
+    using trajecmp::util::r2d;
+    using trajecmp::gesture::estimate_circle_trajectory_average_radius_factor_sized;
+    namespace pm = pattern_matching;
+
+    const auto pattern_trajectory =
+            estimate_circle_trajectory_average_radius_factor_sized(
+                    pm::normalized_size, input_trajectory, mbs);
+    return {
+            input_trajectory,
+            pattern_trajectory,
+            pm::modified_hausdorff_info(
+                    input_trajectory,
+                    pattern_trajectory
+            ),
+    };
+}
+
+model::trajectory get_distance_trajectory(
+        const boost::geometry::distance_info_result<model::point> &distance) {
+    return {
+        distance.projected_point1,
+        distance.projected_point2,
+    };
+}
+
+class framework : public record_trajectory_sdl2_framework {
+    enum class state {
+        match_circle,
+        match_rectangle
+    };
+    state _state = state::match_circle;
+    model::trajectory _beautified_circle_trajectory;
+
+    void handle_input_trajectory(model::trajectory input) override {
+        namespace bg = boost::geometry;
+        using trajecmp::transform::douglas_peucker;
+        using trajecmp::geometry::min_bounding_sphere;
+        using trajecmp::transform::translate_and_scale_using_mbs;
+        using trajecmp::gesture::get_rectangle_comparison_data;
+        using trajecmp::geometry::point::operator-;
+        using pattern_matching::normalized_size;
+
+        // shared preprocessing of input trajectory
+        input = douglas_peucker(3)(input);
+        if (bg::num_points(input) < 3) return;
+        auto mbs = min_bounding_sphere(input);
+        if (_state == state::match_rectangle) {
+            // TODO check size and position are similar to circle
+        }
+        const auto original_mbs = mbs;
+        translate_and_scale_using_mbs(normalized_size, mbs, input);
+        if (_state == state::match_circle) {
+            auto circle_data = get_circle_comparison_data(input, mbs);
+            const bool is_circle = circle_data.distance.real_distance <
+                                      normalized_size * 0.20;
+            if (is_circle) {
+                _beautified_circle_trajectory =
+                        circle_data.preprocessed_pattern_trajectory;
+                trajecmp::transform::scale_and_translate(
+                        original_mbs.radius * 2 / normalized_size,
+                        original_mbs.center,
+                        _beautified_circle_trajectory);
+                renderer_clear();
+                draw_beautified_circle_trajectory();
+                SDL_RenderPresent(_renderer);
+                _state = state::match_rectangle;
+            } else {
+                // TODO show mismatch notification
+            }
+        } else {
+            bg::append(input, *std::begin(input));
+            auto rectangle_data = get_rectangle_comparison_data(
+                    input, pattern_matching::modified_hausdorff_info);
+            const bool is_rectangle = rectangle_data.distance.real_distance <
+                                      normalized_size * 0.20;
+            if (is_rectangle) {
+                renderer_clear();
+                draw_beautified_circle_trajectory();
+                auto beautified_rectangle_trajectory =
+                        rectangle_data.preprocessed_pattern_trajectory;
+                trajecmp::transform::scale_and_translate(
+                        original_mbs.radius * 2 / normalized_size,
+                        original_mbs.center,
+                        beautified_rectangle_trajectory);
+                draw_trajectory(_renderer, beautified_rectangle_trajectory,
+                                color_code::orange);
+                SDL_RenderPresent(_renderer);
+            } else {
+                // TODO show mismatch notification
+            }
+            _state = state::match_circle;
+        }
+        is_rerender(false);
+    }
+
+    void draw_beautified_circle_trajectory()  {
+        draw_trajectory(_renderer, _beautified_circle_trajectory,
+                        color_code::blueViolet);
+    }
+
+    void display() override {
+        record_trajectory_sdl2_framework::display();
+        if (_state == state::match_rectangle) {
+            draw_beautified_circle_trajectory();
+            SDL_RenderPresent(_renderer);
+        }
+    }
+};
+
+int main() {
+    logging::is_logging = true;
+    framework().start();
+    return 0;
+}
